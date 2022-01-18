7710f778c899b4e7960fdad79ec6c03b1e246d3d
glskmc724 <kmc724@zinggls.com>
2022-01-18 09:26:56 +0900

    .

--- Baekjoon/8단계/7. 설탕 배달/7. 설탕 배달/main.c
+++ Baekjoon/8단계/7. 설탕 배달/7. 설탕 배달/main.c
@@ -4,63 +4,36 @@
 int main(void)
 {
 	int weight;
-	int** arr;
+	int* arr;
 	int height;
 
 	scanf("%d", &weight);
-
-	arr = (int**)malloc(sizeof(int*) * (weight + 1));
-	for (int i = 0; i < (weight + 1); i++)
-	{
-		arr[i] = (int*)malloc(sizeof(int) * (weight + 1));
-	}
-
-	for (int i = 0; i < (weight + 1); i++)
-	{
-		for (int j = 0; j < (weight + 1); j++)
-		{
-			arr[i][j] = 0;
-		}
-	}
-
-	arr[0][0] = 3;
-	height = 0;
-	for (int x = 0; x < weight; x++)
-	{
-		for (int y = 0; y < height + 1; y++)
-		{
-			if (y + 1 != weight)
-			{
-				if (arr[y][x + 1] == 0)
-				{
-					arr[y][x + 1] = arr[y][x] + 3;
-					arr[y + 1][x + 1] = arr[y][x] + 5;
-				}
-				else
-				{
-					for (int k = 0; k < weight; k++)
-					{
-						if (arr[k][x + 1] == 0)
-						{
-							height = k;
-							break;
-						}
-					}
-					arr[height][x + 1] = arr[y][x] + 3;
-					arr[height + 1][x + 1] = arr[y][x] + 5;
-				}
-			}
-		}
-		height += 1;
-	}
-
+	arr = (int*)malloc(sizeof(int) * weight);
 	for (int i = 0; i < weight; i++)
 	{
-		for (int j = 0; j < weight; j++)
+		arr[i] = 12345;
+	}
+
+	arr[2] = arr[4] = 1;
+	for (int i = 5; i < weight; i++)
+	{
+		if (arr[i - 3] < arr[i - 5])
 		{
-			printf("%3d ", arr[i][j]);
+			arr[i] = arr[i - 3] + 1;
 		}
-		printf("\n");
+		else
+		{
+			arr[i] = arr[i - 5] + 1;
+		}
+	}
+
+	if (arr[weight - 1] >= 12345)
+	{
+		printf("-1\n");
+	}
+	else
+	{
+		printf("%d\n", arr[weight - 1]);
 	}
 	return 0;
 }
